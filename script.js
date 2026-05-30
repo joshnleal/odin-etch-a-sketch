a3f0def109ef2aecf056ea1fdbad01060e9cbe57
Joshua Leal <118430438+joshnleal@users.noreply.github.com>
2026-05-30 15:43:55 +0200

    Create function createGrid to create a grid of 16 divs*

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.classList = "grid";
-    container.appendChild(newDiv);
-} 
+
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList = "grid";
+        container.appendChild(newDiv);
+    }
+}
+
+createGrid();
