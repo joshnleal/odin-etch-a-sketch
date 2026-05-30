ecc11f45b84aecae11110fedd6c0935bbf90460a
Joshua Leal <118430438+joshnleal@users.noreply.github.com>
2026-05-30 17:02:26 +0200

    Modify the logic of script.js, so that the grid size changes dynamically

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const GRID = 16 * 16;
+let gridWidth = 16; 
+const grid = gridWidth * gridWidth;
+const BLOCK_PIXEL_WIDTH = 25;
+
 const container = document.querySelector(".container");
 
-
 function createGrid() {
-    for (let i = 0; i < GRID; i++) {
+    for (let i = 0; i < grid; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList = "grid";
         newDiv.id = i;
         container.appendChild(newDiv);
     }
+
+    container.style.width = `${BLOCK_PIXEL_WIDTH * gridWidth}px`;
 }
 
 createGrid();
@@ -17,7 +21,5 @@ container.addEventListener("mousemove",(e) => {
     let target = document.getElementById(e.target.id);
 
     target.style.background = "red";
-    // target.addEventListener("mouseleave", () => {
-    //     target.style.background = "white";
-    // });
+
 });
