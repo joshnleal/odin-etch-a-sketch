c687f75dc47f167932f8b6f5d54998f0b19f8ed8
Joshua Leal <118430438+joshnleal@users.noreply.github.com>
2026-05-30 16:15:29 +0200

    Modify script.js to create a 16 x 16 grid by first creating 256 divs

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+const GRID = 16 * 16;
 const container = document.querySelector(".container");
 
+
 function createGrid() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < GRID; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList = "grid";
+        newDiv.id = i;
         container.appendChild(newDiv);
     }
 }
 
 createGrid();
+
+container.addEventListener("mousemove",(e) => {
+    let target = document.getElementById(e.target.id);
+
+    target.style.background = "red";
+    target.addEventListener("mouseleave", () => {
+        target.style.background = "white";
+    });
+});
