fe4b2daf74786708e31298378954fde0e40b4c39
Joshua Leal <118430438+joshnleal@users.noreply.github.com>
2026-05-31 12:32:10 +0200

    Create a deleteGrid function and modify btn listener
    Creating a deleteGrid function will help clear the play area, making the
    createGrid work with a clean slate. Lastly, adjusting the listener to
    call deleteGrid if conditions are met, set new grid values, and createGrid
    will make the website logically sound

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-let gridWidth = 16; 
-let gridHeight = 16;
-let grid = 0;
-setGridValue();
-const BLOCK_PIXEL_WIDTH = 25;
+const BLOCK_PIXEL_WIDTH_AND_HEIGHT = 25;
+const MAX_GRID_WIDTH = 50; // Applies to height as well
+const MIN_GRID_WIDTH = 1; // This too
 
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
 const btn = document.createElement("button");
 btn.textContent = "Reset";
-body.appendChild(btn);
+btn.style.margin = "50px";
+btn.style.padding = "5px 10px";
+btn.style.borderRadius = "8px";
+body.insertBefore(btn, container);        
 
-function setGridValue() {
-    grid = gridWidth * gridHeight;
+// Initialize the first grid (16 x 16)
+let gridWidth = 16; 
+let gridHeight = 16;
+let grid = 0;
+setGridValue();
+createGrid();
+
+function setGridValue() { 
+    grid = gridWidth * gridHeight; 
 }
 
-function getGridValue() {
-    return grid;
+function getGridValue() { 
+    return grid; 
 }
 
 function createGrid() {
@@ -23,13 +31,20 @@ function createGrid() {
         let newDiv = document.createElement("div");
         newDiv.classList = "grid";
         newDiv.id = i;
+        newDiv.style.width = `${BLOCK_PIXEL_WIDTH_AND_HEIGHT}px`;
+        newDiv.style.height = `${BLOCK_PIXEL_WIDTH_AND_HEIGHT}px`;
+        newDiv.style.background = "white";
         container.appendChild(newDiv);
     }
-
-    container.style.width = `${BLOCK_PIXEL_WIDTH * gridWidth}px`;
+    
+    container.style.width = `${BLOCK_PIXEL_WIDTH_AND_HEIGHT * gridWidth}px`;
 }
 
-createGrid();
+function deleteGrid() {
+    for (let i = 0; i < getGridValue(); i++){
+        container.removeChild(container.firstChild);
+    }
+}
 
 container.addEventListener("mousemove",(e) => {
     let target = document.getElementById(e.target.id);
@@ -37,28 +52,31 @@ container.addEventListener("mousemove",(e) => {
     target.style.background = "red";
 });
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
     alert("Create a new grid");
-    
     
     let validInput = false;
     do {
         gridWidth = Number(prompt("Width of the grid (1 - 50)", "16"));
-        console.log(gridWidth < 51 && gridWidth > 0);
-        if (gridWidth == Number && gridWidth < 51 && gridWidth > 0) { 
-            validInput = true; 
-        }
-    } while (!validInput)
-
-    validInput = false;
-    do {
-        gridHeight = Number(prompt("height of the grid (1 - 50)", "16"));
-        if (gridHeight == Number && gridHeight < 51 && gridWidth > 0) { 
-            validInput = true; 
-        }
+        console.log(`width input: ${gridWidth}`);
+        validInput = (gridWidth <= MAX_GRID_WIDTH && gridWidth >= MIN_GRID_WIDTH) 
     } while (!validInput)
         
-    setGridValue();
-    createGrid();
+        validInput = false;
+        do {
+            gridHeight = Number(prompt("Height of the grid (1 - 50)", "16"));
+            console.log(`height input: ${gridHeight}`);
+            validInput = (gridHeight <= MAX_GRID_WIDTH && gridHeight >= MIN_GRID_WIDTH) 
+        } while (!validInput)
+            
+            if (container.hasChildNodes()) { 
+                deleteGrid(); 
+            }
+            
+            // Unfocus button`
+            e.target.blur();
+            
+            setGridValue();
+            
+            createGrid();
 });
-        
